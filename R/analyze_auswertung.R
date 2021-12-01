9a8628d96a79b61802225da5d374ae1baa1ce2fa
Tim <TFangmeyer@gmail.com>
2021-12-01 23:11:45 +0100

    updated code

--- R/analyze_auswertung.R
+++ R/analyze_auswertung.R
@@ -4,10 +4,11 @@ library("xml2")
 filename <- file.choose("Auswertung.rds")
 auswertung <- readRDS(filename)
 
-auswertung <-  auswertung %>% drop_na(kml)
-
 auswertung$xml <- auswertung$kml %>% 
   map(function(x) read_xml(x, as_html = TRUE, options = "NOBLANKS"))
 
-auswertung$coordinates <- auswertung$xml %>% 
-  map(function(x) xml_text(x, "//coordinates"))
+temporary <- auswertung$xml %>% 
+  map(function(x) xml_find_all(x, "//coordinates"))
+
+auswertung$coordinates <- temporary %>% 
+  map(function(x) xml_text(x, trim = FALSE)) 
