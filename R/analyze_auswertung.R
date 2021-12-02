ca313db65b6ca213a200e869d2a30a2c015a69d1
andreas neumann <68635246+anneumann1@users.noreply.github.com>
2021-12-02 03:26:44 +0100

    Update analyze_auswertung.R

--- R/analyze_auswertung.R
+++ R/analyze_auswertung.R
@@ -12,3 +12,15 @@ temporary <- auswertung$xml %>%
 
 auswertung$coordinates <- temporary %>% 
   map(function(x) xml_text(x, trim = FALSE)) 
+      
+
+Auswertung<-Auswertung%>%unnest(coordinates)
+Auswertung$coords<- trimws(Auswertung$coordinates, which = c("both"))
+
+library(stringr)
+
+Auswertung$point <- word(Auswertung$coords, 1:2)
+Auswertung<-Auswertung %>% separate(point, into = c("lon", "lat"), sep = ",")
+AuswertungII<-Auswertung%>%filter(!is.na(lon))
+AuswertungII<- st_as_sf(AuswertungII, coords = c("lon","lat"), crs = 4326)
+AuswertungIII<-AuswertungII[,c(5:8)]      
